58dbc64736eeb90b8fe08a9ee8909137a8d67fa9
Ubuntu <ubuntu@ip-172-31-42-157.ap-south-1.compute.internal>
2025-11-23 11:53:44 +0000

    modified

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const os = require('os');
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 
 
 app.get('/', (req, res) => {
